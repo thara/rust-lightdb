aa37497eb845b0e9fd543487622a3ce20fe4fb28
Tomochika Hara <tomochikahara@gmail.com>
2017-09-12 00:14:11 +0900

    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::io::prelude::*;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("db > ");
+        io::stdout().flush().ok();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().as_ref() {
+            ".exit" => {
+                println!("Bye.");
+                process::exit(0);
+            },
+            s => {
+                println!("Unrecognized command : {}", s);
+            }
+        }
+    }
 }
